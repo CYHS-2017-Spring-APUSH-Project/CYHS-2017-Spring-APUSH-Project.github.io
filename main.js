55bdcd916c74a9a2e4be3134d86aef495975d6cf
CYHS-2017-Spring-APUSH-Project <deadlyfox@comcast.net>
2017-05-09 18:29:00 -0400

    Added centering code for navigation bar
    Javascript allots an equal amount of pixels to each link in the navigation bar.

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+
+function adjustTableWidth() {
+	var x = document.getElementById("navBar").getElementsByTagName("td");
+	var length = document.getElementById("navBar").getElementsByTagName("td").length;
+	
+	for (var i = 0; i < length; i++) {
+		x[i].style.width = (1060/length) + "px";
+		//x[i].style.backgroundColor = "red"; 
+	}     
+}
+
 var iterate = 0;
 		
 function scroll() {
@@ -6,3 +17,7 @@ function scroll() {
 }
 		
 setInterval(scroll, 40);
+
+window.onload = function() {
+  adjustTableWidth();
+};
